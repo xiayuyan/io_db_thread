1c568e8f317567fe82a86e22f5574629a6504b71
夏语嫣 <xia_yu_yan_123@163.com>
2021-07-27 11:50:44 +0800

    创建springboot

---
+++ src/main/java/com/xyy/io_db_thread/IoDbThreadApplication.java
@@ -0,0 +1,13 @@
+package com.xyy.io_db_thread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoDbThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoDbThreadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xyy/io_db_thread/IoDbThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xyy.io_db_thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoDbThreadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
